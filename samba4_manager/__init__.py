267d5899f4b4170e433723485c56335976f06d49
Agustin Villafane <agusvillafane@yahoo.com.ar>
2018-07-28 00:19:02 -0300

    Adding a list of users
    Adding a list of users!!

--- samba4_manager/__init__.py
+++ samba4_manager/__init__.py
@@ -7,7 +7,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_static_view('deform_static', 'deform:static/')
-    config.add_static_view(name='static', path='samba4_manager:static')
+    config.add_static_view('static', 'samba4_manager:static')
     config.add_route('listar_usuarios', '/')
     config.scan()
     return config.make_wsgi_app()
